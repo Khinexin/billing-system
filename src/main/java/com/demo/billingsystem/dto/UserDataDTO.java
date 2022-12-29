c3cc9de2d5a398fc0878577e12857e838b56ac50
Khine Zin <34533081+Khinexin@users.noreply.github.com>
2022-12-29 11:48:47 +0630

    Update UserDataDTO.java

--- src/main/java/com/demo/billingsystem/dto/UserDataDTO.java
+++ src/main/java/com/demo/billingsystem/dto/UserDataDTO.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import com.demo.billingsystem.model.UserRole;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserDataDTO {
   
   private String username;
