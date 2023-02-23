9a815c44adfdb7dfb47da04660de2178aaa54302
Khine Zin <34533081+Khinexin@users.noreply.github.com>
2023-02-23 16:40:03 +0630

    //

--- src/test/java/com/demo/billingsystem/BillingSystemApplicationTest.java
+++ src/test/java/com/demo/billingsystem/BillingSystemApplicationTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
