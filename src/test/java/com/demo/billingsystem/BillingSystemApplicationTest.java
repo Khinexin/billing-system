50912ae8c55975d67364a2e027e32d62fab693e9
Khine Zin <34533081+Khinexin@users.noreply.github.com>
2023-02-23 16:43:48 +0630

    Update BillingSystemApplicationTest.java

--- src/test/java/com/demo/billingsystem/BillingSystemApplicationTest.java
+++ src/test/java/com/demo/billingsystem/BillingSystemApplicationTest.java
@@ -2,14 +2,9 @@ package com.demo.billingsystem;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.demo.billingsystem.model.User;
-import com.demo.billingsystem.model.UserRole;
 import com.demo.billingsystem.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -25,16 +20,6 @@ class BillingSystemApplicationTest {
 		assertThat(userRepository).isNotNull();
 	}
 
-	@Test
-	void testRegister() {
-		
-		List<UserRole> userRoleList = new ArrayList<UserRole>();
-		userRoleList.add(UserRole.ROLE_CLIENT);
-		
-		User user = User.builder()
-				.username("").password("")
-				.userRoles(userRoleList)
-				.build();
-	}
+
 
 }
